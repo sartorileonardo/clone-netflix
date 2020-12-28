714c321e36be3aa2fab61cdb53657c0d04b34514
Leonardo Sartori <leonardogt4@hotmail.com>
2020-12-28 08:49:03 -0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -22,7 +22,7 @@ function mostrarDescricaoCompleta() {
     setTimeout(() => {
         let conteudoDescricaoCompleta = document.getElementById("descricao-completa").innerHTML;
         if (conteudoDescricaoCompleta == "") {
-            document.getElementById("descricao-completa").innerHTML = "Um homem misterioso que atende pelo nome de El Profesor, está planejando o maior assalto do século. A fim de realizar o ambicioso plano ele recruta uma gangue de oito pessoas com certas habilidades que não tem nada a perder. O objetivo é infiltrar na Casa da Moeda, de modo que eles possam imprimir 2,4 bilhões de euros. Para fazer isso eles precisam de onze dias de reclusão, durante o qual eles vão ter que lidar com sessenta e sete reféns e as forças da Polícia de Elite, com cenas de muita ação e planos brilhantes de El Profesor.";
+            document.getElementById("descricao-completa").innerHTML = "Um homem misterioso que atende pelo nome de El Profesor, está planejando o maior assalto do século. A fim de realizar o ambicioso plano ele recruta uma gangue de oito pessoas com certas habilidades que não tem nada a perder.";
         } else {
             document.getElementById("descricao-completa").innerHTML = "";
         }
